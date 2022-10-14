4a317ca4071b595f6a0d5df90f7c762260da58c6
Ahmed <Ahmedvml101@gmail.com>
2022-10-14 12:26:13 -0700

    4.Hello, Puts

--- alx-low_level_programming/0x00-hello_world/4-puts.c
+++ alx-low_level_programming/0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main -Entry Point 
  *
- * Return: Always 0 (Success)
+ * Retur: Always 0 (Success) 
  */
 
-int main(void)
+int main(void) 
 {
-	puts("\"Programming is like building a multi lingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
